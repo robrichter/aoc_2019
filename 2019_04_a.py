4eb930f62a78a2654b81d1ed68cf1f2e8d08a18a
Robert Richter <rori@tmapy.cz>
2019-12-05 05:41:54 +0100

    Add solutions for day 4

---
+++ 2019_04_a.py
@@ -0,0 +1,28 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2019, day=4)
+MIN, MAX = tuple(map(int, puzzle.input_data.split('-')))
+
+passwords = 0
+for number in range(MIN, MAX+1):
+    numbers = str(number)
+    numbers = list(numbers)
+    old_digit = numbers[0]
+    is_valid = False
+    for digit in numbers[1:]:
+        if old_digit == digit:
+            is_valid = True
+            old_digit = digit
+            continue
+        if old_digit > digit:
+            is_valid = False
+            break
+        old_digit = digit
+
+    if is_valid:
+        passwords += 1
+
+
+print(passwords)
+puzzle.answer_a = passwords
+
